9a7858f62602b3a114ce73e3e854cd7171123531
Nullean <mr000lean@gmail.com>
2023-03-03 18:19:11 +0400

    added dal interfaces

---
+++ Nullean.UserOrdersApi.OrdersDao/IOrdersDao.cs
@@ -0,0 +1,10 @@
+﻿using Nullean.UserOrdersApi.Entities;
+
+namespace Nullean.UserOrdersApi.OrdersDao
+{
+    public interface IOrdersDao
+    {
+        public Task<Response<IEnumerable<Order>>> GetUserOrders(Guid id);
+        public Task<Response> AddOrder(Order order, Guid userId);
+    }
+}
---
+++ Nullean.UserOrdersApi.UsersDaoInterface/IUserDao.cs
@@ -0,0 +1,11 @@
+﻿using Nullean.UserOrdersApi.Entities;
+
+namespace Nullean.UserOrdersApi.UsersDaoInterface
+{
+    public interface IUserDao
+    {
+        public Task<Response<User>> GetUserByName(string username);
+        public Task<Response> CreateUser(User user);
+        public Task<Response<UserDetailed>> GetUserDetials(Guid Id);
+    }
+}
