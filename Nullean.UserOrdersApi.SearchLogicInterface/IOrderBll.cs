75c53ec9b05840265624f919d6f1ea99349a8c18
Nullean <mr000lean@gmail.com>
2023-03-03 18:16:09 +0400

    added bll interfaces

---
+++ Nullean.UserOrdersApi.SearchLogicInterface/IOrderBll.cs
@@ -0,0 +1,10 @@
+﻿using Nullean.UserOrdersApi.Entities;
+
+namespace Nullean.UserOrdersApi.SearchLogicInterface
+{
+    public interface IOrderBll
+    {
+        public Task<Response<IEnumerable<Order>>> GetUserOrders(Guid id);
+        public Task<Response> AddOrder(Order order, Guid userId);
+    }
+}
---
+++ Nullean.UserOrdersApi.UsersLogicInterface/IUserBll.cs
@@ -0,0 +1,11 @@
+﻿using Nullean.UserOrdersApi.Entities;
+
+namespace Nullean.UserOrdersApi.UsersLogicInterface
+{
+    public interface IUserBll
+    {
+        public Task<Response<User>> LoginUser(string username, string password);
+        public Task<Response<User>> CreateUser(User user);
+        public Task<Response<UserDetailed>> GetUserDetials(Guid id);
+    }
+}
